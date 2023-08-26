0f094b59b02dff98daa51da06e93441705a6e6a3
xinruoyusixian <xinruoyusixian@163.com>
2023-08-26 23:51:11 +0800

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,7 @@
+@echo on
+taskkill /im adb.exe /f
+taskkill /im shadowsocks-libqss.exe /f
+::notes use the same adb with Vysor, if localhost not work, use 127.0.0.1
+adb.exe reverse tcp:8388 tcp:18630
+%~dp0\shadowsocks-libqss.exe -c %~dp0\config_server.json -S
+pause
